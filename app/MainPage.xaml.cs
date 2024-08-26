7ee130f2d0c798d309f69353ab8e7ea653ffa4c7
Reto Senn <reto.senn@bitforge.ch>
2024-08-26 14:41:53 +0200

    Remove unused code

--- app/MainPage.xaml.cs
+++ app/MainPage.xaml.cs
@@ -43,50 +43,8 @@ public partial class MainPage : ContentPage
 
 	public async Task<RegistrationResponse> Store(IOrderedEnumerable<KeyValuePair<long, string>> sorted)
 	{
-		/*
-		var arr = sorted.ToArray();
-		const int EntriesPerDictionary = 100000; //create separate files for subsets of entries
-		const string delimiter = ";";
-
-		for (int i = 0; i < 1000; i++)
-		{
-
-			var fileName = GetPhoneFilePath(i);
-
-			// Read all existing files
-			if (File.Exists(fileName))
-			{
-				File.Delete(fileName);
-			}
-			else
-			{
-				//Console.WriteLine("Deleted " + i + " files");
-				break;
-			}
-		}
-
-		for (int i=0; i < arr.Length / EntriesPerDictionary+1; i++){
-
-			var fileName = GetPhoneFilePath(i);
-			//var dictionary = new NSMutableDictionary();
-			var lines = new List<string>();
-
-			for (int j = 0; j<EntriesPerDictionary && j+i* EntriesPerDictionary < arr.Length; j++)
-			{
-				var entry = arr[j + i * EntriesPerDictionary];
-				lines.Add($"{entry.Key}{delimiter}{entry.Value}");
-			}
-			File.WriteAllLines(fileName, lines.ToArray());
-
-
-			Console.WriteLine($"AddIdentificationEntry fileName {fileName}");
-			//Console.WriteLine("PhonenumberDirectory" + i);
-
-		}*/
-
 		var taskCompletionSource = new TaskCompletionSource<RegistrationResponse>();
 		var task = taskCompletionSource.Task;
-		//Action<RegistrationResponse> callback = taskCompletionSource.SetResult;
 
 
 		CXCallDirectoryManager.SharedInstance.ReloadExtension(
