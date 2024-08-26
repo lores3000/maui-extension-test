b9309b623ef7f7b732bc702042459149f47421cb
Reto Senn <reto.senn@bitforge.ch>
2024-08-26 14:29:15 +0200

    Project run works with command line

--- app/MainPage.xaml.cs
+++ app/MainPage.xaml.cs
@@ -14,7 +14,7 @@ public partial class MainPage : ContentPage
 		}
 
 		public RegistrationResponseEnum Response { get; set; }
-		public string Reason { get; set; }
+		public string Reason { get; set; } = "";
 	}
 
 	int count = 0;
@@ -24,7 +24,7 @@ public partial class MainPage : ContentPage
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void OnCounterClicked(object sender, EventArgs e)
 	{
 		count++;
 
@@ -34,99 +34,103 @@ public partial class MainPage : ContentPage
 			CounterBtn.Text = $"Clicked {count} times";
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
+
+		var result = await Store(null);
+		Console.WriteLine(result.Response);
+
+		CounterBtn.Text = result.Response.ToString() +" "+ result.Reason;
 	}
 
+	public async Task<RegistrationResponse> Store(IOrderedEnumerable<KeyValuePair<long, string>> sorted)
+	{
+		/*
+		var arr = sorted.ToArray();
+		const int EntriesPerDictionary = 100000; //create separate files for subsets of entries
+		const string delimiter = ";";
 
-        public async Task<RegistrationResponse> Store(IOrderedEnumerable<KeyValuePair<long, string>> sorted)
-        {
-			/*
-            var arr = sorted.ToArray();
-            const int EntriesPerDictionary = 100000; //create separate files for subsets of entries
-            const string delimiter = ";";
+		for (int i = 0; i < 1000; i++)
+		{
 
-            for (int i = 0; i < 1000; i++)
-            {
+			var fileName = GetPhoneFilePath(i);
 
-                var fileName = GetPhoneFilePath(i);
+			// Read all existing files
+			if (File.Exists(fileName))
+			{
+				File.Delete(fileName);
+			}
+			else
+			{
+				//Console.WriteLine("Deleted " + i + " files");
+				break;
+			}
+		}
 
-                // Read all existing files
-                if (File.Exists(fileName))
-                {
-                    File.Delete(fileName);
-                }
-                else
-                {
-                    //Console.WriteLine("Deleted " + i + " files");
-                    break;
-                }
-            }
+		for (int i=0; i < arr.Length / EntriesPerDictionary+1; i++){
 
-            for (int i=0; i < arr.Length / EntriesPerDictionary+1; i++){
+			var fileName = GetPhoneFilePath(i);
+			//var dictionary = new NSMutableDictionary();
+			var lines = new List<string>();
 
-                var fileName = GetPhoneFilePath(i);
-                //var dictionary = new NSMutableDictionary();
-                var lines = new List<string>();
-
-                for (int j = 0; j<EntriesPerDictionary && j+i* EntriesPerDictionary < arr.Length; j++)
-                {
-                    var entry = arr[j + i * EntriesPerDictionary];
-                    lines.Add($"{entry.Key}{delimiter}{entry.Value}");
-                }
-                File.WriteAllLines(fileName, lines.ToArray());
-
-
-                Console.WriteLine($"AddIdentificationEntry fileName {fileName}");
-                //Console.WriteLine("PhonenumberDirectory" + i);
-
-            }*/
-
-            var taskCompletionSource = new TaskCompletionSource<RegistrationResponse>();
-            var task = taskCompletionSource.Task;
-            //Action<RegistrationResponse> callback = taskCompletionSource.SetResult;
+			for (int j = 0; j<EntriesPerDictionary && j+i* EntriesPerDictionary < arr.Length; j++)
+			{
+				var entry = arr[j + i * EntriesPerDictionary];
+				lines.Add($"{entry.Key}{delimiter}{entry.Value}");
+			}
+			File.WriteAllLines(fileName, lines.ToArray());
 
 
-            CXCallDirectoryManager.SharedInstance.ReloadExtension(
-                "ch.usz.internal.directory.callkitextension", 
-                error => 
-                { 
-                    if (error == null) 
-                    {
-                        // Reloaded extension successfully
-                        taskCompletionSource.SetResult(new RegistrationResponse { Response = RegistrationResponse.RegistrationResponseEnum.Ok });
-                    } else {
-                        Console.WriteLine($"Error activating Extension {error.Code}:{error.Description}");
+			Console.WriteLine($"AddIdentificationEntry fileName {fileName}");
+			//Console.WriteLine("PhonenumberDirectory" + i);
 
-                        switch ((CXErrorCodeCallDirectoryManagerError)(int)error.Code)
-                        {
-                            case CXErrorCodeCallDirectoryManagerError.ExtensionDisabled:
-                                taskCompletionSource.SetResult(new RegistrationResponse { Response = RegistrationResponse.RegistrationResponseEnum.NotActivated });
-                                return;
-                            case CXErrorCodeCallDirectoryManagerError.Unknown:
-                            case CXErrorCodeCallDirectoryManagerError.NoExtensionFound:
-                            case CXErrorCodeCallDirectoryManagerError.LoadingInterrupted:
-                            case CXErrorCodeCallDirectoryManagerError.EntriesOutOfOrder:
-                            case CXErrorCodeCallDirectoryManagerError.DuplicateEntries:
-                            case CXErrorCodeCallDirectoryManagerError.MaximumEntriesExceeded:
-                            case CXErrorCodeCallDirectoryManagerError.CurrentlyLoading:
-                            case CXErrorCodeCallDirectoryManagerError.UnexpectedIncrementalRemoval:
-                                Console.WriteLine(error);
-                                taskCompletionSource.SetResult(new RegistrationResponse {
-                                    Response =
-                                    RegistrationResponse.RegistrationResponseEnum.Error,
-                                    Reason = "" + error.Code
-                                });
-                                return;
-                        }
-                        taskCompletionSource.SetResult(
-                            new RegistrationResponse {
-                                Response = RegistrationResponse.RegistrationResponseEnum.Error,
-                                Reason = ""+error.Code
-                            });
-                    }
-                }
-            );
+		}*/
 
-            return await Task.FromResult(task.Result);
-        }
+		var taskCompletionSource = new TaskCompletionSource<RegistrationResponse>();
+		var task = taskCompletionSource.Task;
+		//Action<RegistrationResponse> callback = taskCompletionSource.SetResult;
+
+
+		CXCallDirectoryManager.SharedInstance.ReloadExtension(
+			"ch.bitforge.app.CallDirectoryExtension", 
+			error => 
+			{ 
+				if (error == null) 
+				{
+					// Reloaded extension successfully
+					taskCompletionSource.SetResult(new RegistrationResponse { Response = RegistrationResponse.RegistrationResponseEnum.Ok });
+				} else {
+					Console.WriteLine($"Error activating Extension {error.Code}:{error.Description}");
+
+					switch ((CXErrorCodeCallDirectoryManagerError)(int)error.Code)
+					{
+						case CXErrorCodeCallDirectoryManagerError.ExtensionDisabled:
+							taskCompletionSource.SetResult(new RegistrationResponse { Response = RegistrationResponse.RegistrationResponseEnum.NotActivated });
+							return;
+						case CXErrorCodeCallDirectoryManagerError.Unknown:
+						case CXErrorCodeCallDirectoryManagerError.NoExtensionFound:
+						case CXErrorCodeCallDirectoryManagerError.LoadingInterrupted:
+						case CXErrorCodeCallDirectoryManagerError.EntriesOutOfOrder:
+						case CXErrorCodeCallDirectoryManagerError.DuplicateEntries:
+						case CXErrorCodeCallDirectoryManagerError.MaximumEntriesExceeded:
+						case CXErrorCodeCallDirectoryManagerError.CurrentlyLoading:
+						case CXErrorCodeCallDirectoryManagerError.UnexpectedIncrementalRemoval:
+							Console.WriteLine(error);
+							taskCompletionSource.SetResult(new RegistrationResponse {
+								Response =
+								RegistrationResponse.RegistrationResponseEnum.Error,
+								Reason = "" + error.Code
+							});
+							return;
+					}
+					taskCompletionSource.SetResult(
+						new RegistrationResponse {
+							Response = RegistrationResponse.RegistrationResponseEnum.Error,
+							Reason = ""+error.Code
+						});
+				}
+			}
+		);
+
+		return await Task.FromResult(task.Result);
+	}
 }
 
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Test project for ios app extension. Does not run in VS Code, runs with command lines:
+
+## Get Device ID:
+xcrun xctrace list devices
+
+## Build:
+dotnet build /p:RuntimeIdentifier=ios-arm64 /bl -f net8.0-ios
+
+## Run: (use device name from xcrun)
+/usr/local/share/dotnet/packs/Microsoft.iOS.Sdk/17.2.8053/tools/bin/mlaunch --installdev bin/Debug/net8.0-ios/ios-arm64/app.app --devname=00008110-000E14590E42801E
